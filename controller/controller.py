6a089593187f80d7a4ed45b682c54820c1c44fa1
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-12-07 16:44:35 +0000

    Add controller

---
+++ controller/controller.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import os
+import sys
+
+import grpc
+from flask import Flask, jsonify, request
+from node_manager import NodeManager
+from switch_controller import SwitchController
+
+app = Flask(__name__)
+
+global nodeManager
+
+
+def main(config_file_path):
+    try:
+        with open(config_file_path, "r") as config_file:
+            switch_configs = json.load(config_file)
+
+        switch_controllers = []
+        master_config = None
+
+        for config in switch_configs:
+            # Master needs to be initialized last,
+            # otherwise performing master arbitration update will fail
+            if config.get("master", False):
+                if master_config is not None:
+                    raise Exception(
+                        "Multiple master switches specified "
+                        "in the configuration file."
+                    )
+                master_config = config
+                continue
+
+            switch_controller = SwitchController(
+                p4info_file_path=config["p4info_file_path"],
+                bmv2_file_path=config["bmv2_file_path"],
+                sw_name=config["name"],
+                sw_addr=config["addr"],
+                sw_id=config["id"],
+                proto_dump_file=config["proto_dump_file"],
+                initial_table_rules_file=config["runtime_file"],
+            )
+            switch_controllers.append(switch_controller)
+
+        if master_config is None:
+            raise Exception(
+                "No master switch specified" "in the configuration file."
+            )
+        lb_nodes = master_config.get("lb_nodes", None)
+
+        master_controller = SwitchController(
+            p4info_file_path=master_config["p4info_file_path"],
+            bmv2_file_path=master_config["bmv2_file_path"],
+            sw_name=master_config["name"],
+            sw_addr=master_config["addr"],
+            sw_id=master_config["id"],
+            proto_dump_file=master_config["proto_dump_file"],
+            initial_table_rules_file=master_config["runtime_file"],
+        )
+
+        global nodeManager
+        nodeManager = NodeManager(
+            switch_controller=master_controller, lb_nodes=lb_nodes
+        )
+
+    except KeyboardInterrupt:
+        print("Shutting down.")
+    except grpc.RpcError as e:
+        printGrpcError(e)
+        exit(1)
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
+
+    app.run(port=5000)
+
+
+def printGrpcError(e):
+    print("gRPC Error:", e.details(), end=" ")
+    status_code = e.code()
+    print("(%s)" % status_code.name, end=" ")
+    traceback = sys.exc_info()[2]
+    if traceback:
+        print(
+            "[%s:%d]"
+            % (traceback.tb_frame.f_code.co_filename, traceback.tb_lineno)
+        )
+
+
+@app.route("/update_node", methods=["POST"])
+def update_node():
+    data = request.get_json()
+
+    old_ipv4 = data.get("old_ipv4")
+    new_ipv4 = data.get("new_ipv4")
+    dest_mac = data.get("dmac")
+
+    try:
+        egress_port = int(data.get("eport"))
+    except ValueError:
+        return jsonify({"error": "Invalid eport parameter"}), 400
+
+    if not all([old_ipv4, new_ipv4, dest_mac, egress_port]):
+        return jsonify({"error": "Missing parameters"}), 400
+
+    try:
+        nodeManager.updateNode(old_ipv4, new_ipv4, dest_mac, egress_port)
+        return jsonify({"status": "success"}), 200
+    except grpc.RpcError as e:
+        printGrpcError(e)
+        return jsonify({"error": str(e)}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="P4Runtime Controller")
+    parser.add_argument(
+        "--config",
+        help="JSON configuration file for switches",
+        type=str,
+        action="store",
+        required=True,
+    )
+    args = parser.parse_args()
+
+    if not os.path.exists(args.config):
+        parser.print_help()
+        print(f"\nConfiguration file not found: {args.config}")
+        parser.exit(1)
+    main(args.config)
---
+++ controller/switch_controller.py
@@ -0,0 +1,185 @@
+# Import P4Runtime lib from parent utils dir
+# Probably there's a better way of doing this.
+import json
+
+import p4runtime_lib.bmv2
+import p4runtime_lib.helper
+from p4runtime_lib.simple_controller import program_from_file
+from p4runtime_lib.switch import ShutdownAllSwitchConnections
+
+
+class SwitchController(object):
+    def __init__(
+        self,
+        p4info_file_path,
+        bmv2_file_path,
+        sw_name,
+        sw_addr,
+        sw_id,
+        proto_dump_file,
+        initial_table_rules_file=None,
+    ):
+        self.p4info_file_path = p4info_file_path
+        self.bmv2_file_path = bmv2_file_path
+        self.sw_name = sw_name
+        self.sw_addr = sw_addr
+        self.sw_id = sw_id
+        self.proto_dump_file = proto_dump_file
+        self.initial_table_rules_file = initial_table_rules_file
+
+        self.p4info_helper = p4runtime_lib.helper.P4InfoHelper(p4info_file_path)
+        self.sw = p4runtime_lib.bmv2.Bmv2SwitchConnection(
+            name=sw_name,
+            address=sw_addr,
+            device_id=sw_id,
+            proto_dump_file=proto_dump_file,
+        )
+        self.initial_table_rules_file = initial_table_rules_file
+
+        # Send master arbitration update message
+        # to establish this controller as
+        # master (required by P4Runtime before
+        # performing any other write operation)
+        self.sw.MasterArbitrationUpdate()
+
+        # Install the P4 program on the switches
+        self.sw.SetForwardingPipelineConfig(
+            p4info=self.p4info_helper.p4info,
+            bmv2_json_file_path=bmv2_file_path,
+        )
+        print(
+            f"Installed P4 Program using"
+            f"SetForwardingPipelineConfig on {sw_name}"
+        )
+
+        if initial_table_rules_file:
+            with open(initial_table_rules_file, "r") as sw_conf_file:
+                sw_conf = json.load(sw_conf_file)
+                program_from_file(
+                    sw=self.sw,
+                    sw_conf=sw_conf,
+                    p4info_helper=self.p4info_helper,
+                    runtime_json=None,
+                )
+
+    def __del__(self):
+        ShutdownAllSwitchConnections()
+
+    def insertEcmpGroupSelectEntry(
+        self, matchDstAddr, ecmp_base, ecmp_count, update_type="INSERT"
+    ):
+        table_entry = self.p4info_helper.buildTableEntry(
+            table_name="MyIngress.ecmp_group",
+            match_fields={"hdr.ipv4.dstAddr": matchDstAddr},
+            action_name="MyIngress.set_ecmp_select",
+            action_params={"ecmp_base": ecmp_base, "ecmp_count": ecmp_count},
+        )
+        self.sw.WriteTableEntry(table_entry, update_type=update_type)
+        print(
+            f"Updated the 'ecmp_group' table on "
+            f"{self.sw.name=} with {matchDstAddr=}, {ecmp_base=}, {ecmp_count=}"
+        )
+
+    def insertEcmpGroupRewriteSrcEntry(
+        self, matchDstAddr, new_src, update_type="INSERT"
+    ):
+        table_entry = self.p4info_helper.buildTableEntry(
+            table_name="MyIngress.ecmp_group",
+            match_fields={"hdr.ipv4.dstAddr": matchDstAddr},
+            action_name="MyIngress.set_rewrite_src",
+            action_params={"new_src": new_src},
+        )
+        self.sw.WriteTableEntry(table_entry, update_type=update_type)
+        print(
+            f"Updated the 'ecmp_group' table on "
+            f"{self.sw.name=} with {matchDstAddr=}, {new_src=}"
+        )
+
+    def insertEcmpNhopEntry(
+        self, ecmp_select, dmac, ipv4, port, update_type="INSERT"
+    ):
+        table_entry = self.p4info_helper.buildTableEntry(
+            table_name="MyIngress.ecmp_nhop",
+            match_fields={"meta.ecmp_select": ecmp_select},
+            action_name="MyIngress.set_nhop",
+            action_params={"nhop_dmac": dmac, "nhop_ipv4": ipv4, "port": port},
+        )
+        self.sw.WriteTableEntry(table_entry, update_type=update_type)
+        print(
+            f"Updated the 'ecmp_nhop' table on "
+            f"{self.sw.name=} with {ecmp_select=}, {ipv4=}, {dmac=}, {port=}"
+        )
+
+    def deleteEcmpNhopEntry(self, ecmp_select):
+        table_entry = self.p4info_helper.buildTableEntry(
+            table_name="MyIngress.ecmp_nhop",
+            match_fields={"meta.ecmp_select": ecmp_select},
+        )
+        self.sw.WriteTableEntry(table_entry, update_type="DELETE")
+        print(
+            f"Deleted a 'ecmp_nhop' table entry on"
+            f"{self.sw.name=} with {ecmp_select=}"
+        )
+
+    def insertSendFrameEntry(self, egress_port, smac, update_type="INSERT"):
+        table_entry = self.p4info_helper.buildTableEntry(
+            table_name="MyEgress.send_frame",
+            match_fields={"standard_metadata.egress_port": egress_port},
+            action_name="MyEgress.rewrite_mac",
+            action_params={"smac": smac},
+        )
+        self.sw.WriteTableEntry(table_entry, update_type)
+        print(
+            f"Updated the 'send_frame' table on "
+            f"{self.sw.name=} with {egress_port=}, {smac=}"
+        )
+
+    def deleteSendFrameEntry(self, egress_port):
+        table_entry = self.p4info_helper.buildTableEntry(
+            table_name="MyEgress.send_frame",
+            match_fields={"standard_metadata.egress_port": egress_port},
+        )
+        self.sw.WriteTableEntry(table_entry, update_type="DELETE")
+        print(
+            f"Deleted a 'send_frame' table entry on"
+            f"{self.sw.name=} with {egress_port=}"
+        )
+
+    def readTableRules(self):
+        """
+        Reads the table entries from all tables on the switch.
+
+        :param p4info_helper: the P4Info helper
+        :param sw: the switch connection
+        """
+        print("\n----- Reading tables rules for %s -----" % self.sw.name)
+        for response in self.sw.ReadTableEntries():
+            for entity in response.entities:
+                entry = entity.table_entry
+                table_name = self.p4info_helper.get_tables_name(entry.table_id)
+                print("%s: " % table_name, end=" ")
+                for m in entry.match:
+                    print(
+                        self.p4info_helper.get_match_field_name(
+                            table_name, m.field_id
+                        ),
+                        end=" ",
+                    )
+                    print(
+                        "%r" % (self.p4info_helper.get_match_field_value(m),),
+                        end=" ",
+                    )
+                action = entry.action.action
+                action_name = self.p4info_helper.get_actions_name(
+                    action.action_id
+                )
+                print("->", action_name, end=" ")
+                for p in action.params:
+                    print(
+                        self.p4info_helper.get_action_param_name(
+                            action_name, p.param_id
+                        ),
+                        end=" ",
+                    )
+                    print("%r" % p.value, end=" ")
+                print()
